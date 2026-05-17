b4ad06905eb50084f8cf6e609eb2d295672cf8fe
melissa-ribeiro <melissa.ribeiro@sptech.school>
2026-05-17 02:30:50 -0300

    First commit - Dashboard Biblioteca Pessoal

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,138 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function autenticar(req, res) {
+
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+
+    } 
+    else {
+
+        usuarioModel.autenticar(email, senha)
+
+            .then(function (resultadoAutenticar) {
+
+                console.log(resultadoAutenticar);
+
+                if (resultadoAutenticar.length == 1) {
+
+                    res.json({
+                        id_usuario: resultadoAutenticar[0].id_usuario,
+                        nome: resultadoAutenticar[0].nome,
+                        email: resultadoAutenticar[0].email,
+                        primeiro_acesso: resultadoAutenticar[0].primeiro_acesso,
+                        nickname: resultadoAutenticar[0].nickname,
+                        pronome: resultadoAutenticar[0].pronome,
+                        avatar: resultadoAutenticar[0].avatar
+                    });
+
+                } else if (resultadoAutenticar.length == 0) {
+
+                    res.status(403).send("Email e/ou senha inválido(s)");
+
+                } else {
+
+                    res.status(403).send("Mais de um usuário com o mesmo login!");
+
+                }
+
+            }).catch(function (erro) {
+
+                console.log(erro);
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+
+                res.status(500).json(erro.sqlMessage);
+
+            });
+    }
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    console.log("SENHA RECEBIDA NO CONTROLLER:", senha);
+    // Faça as validações dos valores
+    if (nome == undefined) {
+
+        res.status(400).send("Seu nome está undefined!");
+
+    } else if (email == undefined) {
+
+        res.status(400).send("Seu email está undefined!");
+
+    } else if (senha == undefined) {
+
+        res.status(400).send("Sua senha está undefined!");
+
+    } else {
+
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+function onboarding(req, res) {
+    // variaveis para recuperar os valores do arquivo boasVindas.html
+    var nickname = req.body.nicknameServer;
+    var pronome = req.body.pronomeServer;
+    var avatar = req.body.avatarServer;
+    var idUsuario = req.body.idUsuarioServer;
+
+    if (nickname == undefined) {
+        res.status(400).send("Seu apelido está undefined!");
+    } else if (pronome == undefined) {
+        res.status(400).send("Seus pronomes estão undefined!");
+    } else if (avatar == undefined) {
+        res.status(400).send("Seu avatar está undefined!");
+    } else if (idUsuario == undefined) {
+        res.status(400).send("Seu ID está undefined!");
+    } else {
+
+        usuarioModel.onboarding(nickname, pronome, avatar, idUsuario)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    onboarding
+}
---
+++ src/models/usuarioModel.js
@@ -0,0 +1,69 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+    SELECT 
+        id_usuario, 
+        nome, 
+        email, 
+        primeiro_acesso,
+        nickname,
+        pronome,
+        avatar
+    FROM usuario 
+    WHERE email = '${email}' 
+    AND senha = sha2('${senha}', 256);
+`;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha) {
+    console.log("SENHA RECEBIDA NO MODEL:", senha);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (nome,email, senha) VALUES ('${nome}','${email}',  sha2('${senha}', 256));
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function onboarding(nickname, pronome, avatar, idUsuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nickname, pronome, avatar);
+    // PRIMEIRO O UPDATE
+    var instrucaoUpdate = `
+    UPDATE usuario 
+    SET avatar = '${avatar}',
+        nickname = '${nickname}',
+        pronome = '${pronome}',
+        primeiro_acesso = 0
+    WHERE id_usuario = ${idUsuario};
+`;
+    console.log("Executando a instrução SQL: \n" + instrucaoUpdate);
+
+    return database.executar(instrucaoUpdate).then(() => {
+        // SELECT PARA RETORNAR DADOS ATUALIZADOS
+    var instrucaoSelect = `
+    SELECT nickname,
+    pronome,
+    avatar
+    FROM usuario 
+    WHERE id_usuario = ${idUsuario}
+    `;
+        console.log("Executando a instrução SQL: \n" + instrucaoSelect);
+        return database.executar(instrucaoSelect);
+    });
+
+
+}
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    onboarding
+};
